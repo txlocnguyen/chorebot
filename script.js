8cecc5ffe3367f31622c2a742f304a8636a08e72
Loc Nguyen <txlocnguyen@gmail.com>
2022-01-31 12:51:40 -0600

    add random factor of chorebot appears behind door

--- script.js
+++ script.js
@@ -4,12 +4,35 @@ let doorImage3 = document.getElementById('door3');
 let botDoorPath = "./assets/robot.svg";
 let beachDoorPath = "./assets/beach.svg";
 let spaceDoorPath = "./assets/space.svg";
-doorImage1.onclick = () =>{
-    doorImage1.src = botDoorPath;
+let numClosedDoor = 3;
+let openDoor1, openDoor2, openDoor3;
+const randomChoreDoorGenerator = () => {
+    const choreDoor = Math.floor(Math.random() * numClosedDoor);
+    switch (choreDoor) {
+        case 0:
+            openDoor1 = botDoorPath;
+            openDoor2 = beachDoorPath;
+            openDoor3 = spaceDoorPath;
+            break;
+        case 1:
+            openDoor2 = botDoorPath;
+            openDoor1 = beachDoorPath;
+            openDoor3 = spaceDoorPath;
+            break;
+        case 2:
+            openDoor3 = botDoorPath;
+            openDoor1 = beachDoorPath;
+            openDoor2 = spaceDoorPath;
+            break;
+    }
 }
-doorImage2.onclick = () =>{
-    doorImage2.src = beachDoorPath;
+doorImage1.onclick = () => {
+    doorImage1.src = openDoor1;
 }
-doorImage3.onclick = () =>{
-    doorImage3.src = spaceDoorPath;
+doorImage2.onclick = () => {
+    doorImage2.src = openDoor2;
 }
+doorImage3.onclick = () => {
+    doorImage3.src = openDoor3;
+}
+randomChoreDoorGenerator();
